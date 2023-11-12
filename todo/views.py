aaa10dd5e5d6a44f21a1ba6384d7431061dc3810
hiroya <hiroya3508@gmail.com>
2023-11-12 17:08:17 +0900

    ListViewの実装完了

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import TodoModel
 
-# Create your views here.
+class TodoList(ListView):
+    template_name = 'list.html'
+    model = TodoModel

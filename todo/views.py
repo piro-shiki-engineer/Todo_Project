3c964a7d60499377486fe5aff3404a8f268dd043
hiroya <hiroya3508@gmail.com>
2023-11-15 19:28:14 +0900

    CreateViewを使ったPOST実装

--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 from .models import TodoModel
+
 
 class TodoList(ListView):
     template_name = 'list.html'
     model = TodoModel
 
+
 class TodoDetail(DetailView):
     template_name = 'detail.html'
     model = TodoModel
+
+
+class TodoCreate(CreateView):
+    template_name = 'create.html'
+    model = TodoModel
+    fields = ('title', 'memo', 'priority', 'duedate')
+    success_url = reverse_lazy('list')
